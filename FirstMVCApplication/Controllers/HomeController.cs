351af6f41439ec4bd17c629ad14e82df97d9b87b
Admin <Admin@DESKTOP-2UK8P1N>
2022-03-23 00:24:05 +0530

    Add project files.

---
+++ FirstMVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMVCApplication.Models
+
+namespace FirstMVCApplication.Controllers;
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            Provider p = new Provider();
+
+            p.ProviderId = 1;
+            p.ProviderName = "Upadhyay";
+            p.ProviderType = "Claim";
+
+            ViewBag.Message = "Good Morning Vikas Upadhyay";
+            return View(p);
+        }
+    }
+}
---
+++ FirstMVCApplication/Models/Provider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVCApplication.Models
+{
+    public class Provider
+    {
+        public int ProviderId { get; set; }
+        public string ProviderName { get; set; }
+        public string ProviderType { get; set; }
+    }
+}
